f8f9507ed07413a1d659df5ea3bd48d8491d28ac
s-liujef <s-liujef@53S50031104.it.bsd405.org>
2019-09-23 08:46:34 -0700

    Turning in project 1

--- ASCIIArt/src/ASCIIArt.java
+++ ASCIIArt/src/ASCIIArt.java
@@ -1,27 +1,82 @@
-/**
- * Unit 1 Project
- * 
- * In this project you have the opportunity to create a piece of artwork using ASCII 
- * (American Standard Code for Information Interchange). Using the already created file 
- * named `ASCIIArt.java` write a program that outputs your artwork to the console. 
- * The goal of this project is to use ONLY what we have learned in Unit 1 to create 
- * anything you would like so be creative (and school appropriate). Artwork must also have a caption describing what it's depicting.  
- * 
- * For those of you who have dabbled in coding before, try to contain your code to 
- * just what we have discussed in this unit.  
- * 
- * @author Mr. Rasmussen 
- *
- */
-
-// YOU CODE HERE :) 
 
 public class ASCIIArt {
-	public static void main (String [] args) {
-		System.out.println("THIS IS MY ASCII ART!!!!");
-		System.out.println("THIS IS MY ASCII ART!!!!");
-		System.out.println("THIS IS MY ASCII ART!!!!");
-		System.out.println("THIS IS MY ASCII ART!!!!");
-		System.out.println("THIS IS MY ASCII ART!!!!");
+
+	public static void fiveDollars () {
+		System.out.print("$$$$$");
 	}
-}
+	public static void twoDollars () {
+		System.out.print("$$");
+	}
+	public static void oneDollar () {
+		System.out.print("$");
+	}
+	public static void oneSpace () {
+		System.out.print(" ");
+	}	
+	public static void threeSpaces () {
+		System.out.print("   ");
+	}
+	public static void fiveSpaces () {
+		System.out.print("     ");
+	}
+	public static void sixSpaces () {
+		System.out.print("      ");
+	}
+	public static void sevenSpaces () {
+		System.out.print("       ");
+	}
+	public static void eightSpaces () {
+		System.out.print("        ");
+	}
+	public static void nineSpaces () {
+		System.out.print("         ");
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("\t\\THE \"OLYMPIC\" RINGS\\");System.out.println(" ");
+		
+		threeSpaces();fiveDollars();sevenSpaces();fiveDollars();
+		sevenSpaces();fiveDollars();System.out.println(" ");
+		
+		oneSpace();twoDollars();fiveSpaces();twoDollars();
+		threeSpaces();twoDollars();fiveSpaces();twoDollars();
+		threeSpaces();twoDollars();fiveSpaces();twoDollars();System.out.println(" ");
+		
+		oneDollar();nineSpaces();oneDollar();oneSpace();oneDollar();
+		nineSpaces();oneDollar();oneSpace();oneDollar();
+		nineSpaces();oneDollar();System.out.println(" ");
+		
+		oneDollar();eightSpaces();fiveDollars();sevenSpaces();
+		fiveDollars();eightSpaces();oneDollar();System.out.println(" ");
+		
+		oneDollar();sixSpaces();twoDollars();oneSpace();oneDollar();
+		oneSpace();oneDollar();oneSpace();twoDollars();threeSpaces();
+		twoDollars();oneSpace();oneDollar();oneSpace();oneDollar();
+		oneSpace();twoDollars();sixSpaces();System.out.println(" ");
+		
+		oneSpace();twoDollars();threeSpaces();oneDollar();oneSpace();
+		twoDollars();threeSpaces();twoDollars();oneSpace();oneDollar();oneSpace();oneDollar();
+		oneSpace();twoDollars();threeSpaces();twoDollars();oneSpace();oneDollar();
+		threeSpaces();twoDollars();System.out.println(" ");
+		
+		threeSpaces();fiveDollars();sevenSpaces();fiveDollars();
+		sevenSpaces();fiveDollars();System.out.println(" ");
+		
+		sixSpaces();oneDollar();nineSpaces();oneSpace();oneDollar();
+		nineSpaces();oneDollar();System.out.println(" ");
+		
+		sevenSpaces();twoDollars();fiveSpaces();twoDollars();threeSpaces();
+		twoDollars();fiveSpaces();twoDollars();System.out.print("\n");
+		
+		eightSpaces();fiveDollars();sevenSpaces();fiveDollars();
+		System.out.println(" ");System.out.println(" ");
+		
+		System.out.print("\t   \\TOKYO 2020\\");
+		
+	}	
+	}
+    
+
+
+
